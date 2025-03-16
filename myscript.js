c5102f71305fcf49a8efee3b8b33ea87c151ae20
jkchoi2 <send_jk@nate.com>
2025-03-17 00:52:01 +0900

    second

--- myscript.js
+++ myscript.js
@@ -1,5 +1,12 @@
 var gui = require("nw.gui");
 
+// Extend application menu for Mac OS
+if (process.platform == "darwin") {
+  var menu = new gui.Menu({type: "menubar"});
+  menu.createMacBuiltin && menu.createMacBuiltin(window.document.title);
+  gui.Window.get().menu = menu;
+}
+
 window.onfocus = function() { 
   console.log("focus");
 };
@@ -23,4 +30,32 @@ window.onload = function() {
     win.showDevTools();
     this.innerText = "Developer Tools";
   };
+  gui.Window.get().show();
+};
+
+
+function make_menu()
+{
+  // Create an empty context menu
+  var menu = new nw.Menu();
+
+  // Add some items with label
+  menu.append(new nw.MenuItem({
+    label: 'Item A',
+    click: function() { 
+      alert('You have clicked at "Item A"');
+      console.log('clicked.');
+    }
+  }));
+
+  menu.append(new nw.MenuItem({ label: 'Item BB' }));
+  menu.append(new nw.MenuItem({ type: 'separator' }));
+  menu.append(new nw.MenuItem({ label: 'Item C' }));
+  // Hooks the "contextmenu" event
+  document.body.addEventListener('contextmenu', function(ev) {
+    ev.preventDefault();    // Prevent showing default context menu
+    menu.popup(ev.x, ev.y); // Popup the native context menu at place you click
+    return false;
+  }, false);  
+
 };
